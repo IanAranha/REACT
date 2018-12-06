359e258758ecac7acb41fe98781c44aa4a03ae08
Ian Aranha <ianorama@gmail.com>
2018-12-06 13:16:15 -0800

    Second React project

---
+++ Project2/Adder.js
@@ -0,0 +1,11 @@
+function Adder({n1, n2}) {
+    const sum = n1 + n2;
+    return React.createElement('h1', {}, sum);
+}
+
+ReactDOM.render(
+    React.createElement(Adder, { n1: 2, n2: 4 }),
+    document.getElementById('app')
+)
+
+//Adder takes an object and returns a React element.
