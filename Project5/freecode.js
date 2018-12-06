29fe1a3d7cc3464b7073d18b8bb7da07a14070ad
Ian Aranha <ianorama@gmail.com>
2018-12-06 15:32:36 -0800

    Second Freecode challenge.

---
+++ Project5/freecode.js
@@ -0,0 +1,11 @@
+const JSX = <div>
+                <h1>Learning JSX</h1>
+                <p>This JSX stuff is awesome</p>
+                <ul>
+                    <li>Fast</li>
+                    <li>Smooth</li>
+                    <li>Complicated</li>
+                </ul>
+            </div>
+
+ReactDOM.render(JSX, document.getElementById('app'))
