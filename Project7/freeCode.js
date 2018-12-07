9170f8f99b634830f76e1490dc033ecbe84b6af4
Ian Aranha <ianorama@gmail.com>
2018-12-06 17:17:06 -0800

    Challenge #4 from FreeCode React.

---
+++ Project7/freeCode.js
@@ -0,0 +1,10 @@
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+    <p>This was a boring challenge which just built upon previous work</p>
+    <p>I still did it to show that I know how to pass HTML to front page.</p>
+  </div>
+);
+
+ReactDOM.render(JSX, document.getElementById('app'));
