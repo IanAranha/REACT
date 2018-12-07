012280c0957e8893b61f69772576ed470e572c84
Ian Aranha <ianorama@gmail.com>
2018-12-07 15:01:00 -0800

    How to overide a default class setting.

--- Project19/freeCode.js
+++ Project19/freeCode.js
@@ -1,10 +1,10 @@
 const ShoppingCart = (props) => {
-  ShoppingCart.defaultProps = {items: 0}
   return (
       <div>
         <h1>Shopping Cart Component</h1>
       </div>
   )
 }
+ShoppingCart.defaultProps = {items: 0}
 
 ReactDOM.render(<ShoppingCart/>,document.getElementById('app'));
