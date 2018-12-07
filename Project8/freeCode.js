73314c64e7a9791547e04ca84f41548258c0d6ff
Ian Aranha <ianorama@gmail.com>
2018-12-06 17:31:29 -0800

    Adding className to an element in JSX.

---
+++ Project8/freeCode.js
@@ -0,0 +1,8 @@
+const JSX = (
+  <div className='myDiv'>
+    <h1>Add a class to this div</h1>
+    <p>This exercise demonstrates how to add 'className' to an element.</p>
+  </div>
+)
+
+ReactDOM.render(JSX, document.getElementById('app'));
