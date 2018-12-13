472ef32f6504cd9261e6231ec06398c5858986b9
Ian Aranha <ianorama@gmail.com>
2018-12-12 19:42:27 -0800

    Added a toggleswitch with and if/else statement.

---
+++ Project39/freeCode.js
@@ -0,0 +1,35 @@
+class MyComponent extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            display: true
+        }
+        
+        this.toggleDisplay = this.toggleDisplay.bind(this)
+    }
+    
+    toggleDisplay(){
+        this.setState({
+            display: !this.state.display
+        })
+    }
+    
+    render(){
+        if(this.state.display === true){
+            return(
+                <div>
+                    <button onClick={this.toggleDisplay}>Toggle Display</button>
+                    <h1>Stuff Displayed</h1>
+                </div>
+            )
+        } else {
+            return(
+                <div>
+                    <button onClick={this.toggleDisplay}>Toggle Display</button>
+                    <h1>Stuff Hidden</h1>
+                </div>
+            ) 
+        }
+    }
+}
+ReactDOM.render(<MyComponent />, document.getElementById('app'))
